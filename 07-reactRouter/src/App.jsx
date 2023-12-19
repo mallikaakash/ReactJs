ba266ab5c5b4f2b9444792b3ec5e2f9dd6aec446
Aakash Mallik <121702227+mallikaakash@users.noreply.github.com>
2023-12-19 12:42:55 +0530

    commiting reactRouter

---
+++ 07-reactRouter/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header/Header.jsx'
+import React from 'react'
+import Footer from './components/Footer/Footer.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  )
+}
+
+export default App
