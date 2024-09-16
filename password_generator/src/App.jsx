f35bfa44abc0784ded9bdfd3122ceed202ebed4b
mallikaakash <mallik.aakash777@gmail.com>
2024-09-16 19:58:26 +0530

    fin passowordgenerator

--- password_generator/src/App.jsx
+++ password_generator/src/App.jsx
@@ -1,4 +1,9 @@
-import { useCallback, useState, useEffect } from 'react'
+/* 
+This project is a simple password generator that generates a password based on the user's input. The user can choose the length of the password, whether to include numbers, and whether to include symbols. The password is generated using the Math.random() function and the charset of characters that the user chooses.
+The main focus of this project is to learn about the useState, useEffect, and useCallback hooks in React. The useState hook is used to manage the state of the password, length, numbers, and symbols. The useEffect hook is used to generate the password when the component is mounted and when the length, numbers, and symbols change. The useCallback hook is used to prevent the function from being re-created every time the component re-renders. This is useful when the function is passed as a prop to a child component.
+*/
+
+import { useCallback, useState, useEffect,useRef } from 'react'
 import './App.css'
 
 function App() {
@@ -7,7 +12,10 @@ function App() {
   const [numbers,setNumbers] = useState(false)
   const [symbols,setSymbols] = useState(false)
 
-  const PasswordGenerator= useCallback(()=> {
+  //UseRef hook
+  const passwordRef = useRef(null) // make it seem better 
+
+  const PasswordGenerator= useCallback(()=> { //useCallback is used to prevent the function from being re-created every time the component re-renders. This is useful when the function is passed as a prop to a child component.
     let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     if(numbers) charset += '0123456789'
     if(symbols) charset += '!@#$%^&*()_+'
@@ -18,9 +26,17 @@ function App() {
     setPassword(password)
   }, [length,numbers,symbols])
 
+const copyPasswordToClip = useCallback (() => {
+  passwordRef.current?.select();
+  // passwordRef.current?.setSelectionRange(0,3);
+    window.navigator.clipboard.writeText(password)
+}, [password])
+
   useEffect(()=>{
     PasswordGenerator()
-  }, [length,numbers,symbols,PasswordGenerator])
+  }, [length,numbers,symbols])
+  //Putting PasswordGenerator in the useEffect will make sure that the password is generated when the component is mounted and when the length, numbers, and symbols change.
+  //Putting Password here traps it in an infinite loop because it will keep updating the state and re-rendering the component.
 
   return (
     <>
@@ -28,8 +44,8 @@ function App() {
       <h1 className='text-white text-3xl '> Password Generator</h1>
 
       <div>
-        <input type="text" value={password} readOnly className='text-black text-2xl bg-white w-96 h-10 rounded-md mr-8' placeholder='Password'/>
-        <button className='bg-blue-500 text-white w-20 h-10 rounded-md'>Generate</button>
+        <input type="text" value={password} readOnly className='text-black text-2xl bg-white w-96 h-10 rounded-md ' placeholder='Password' ref={passwordRef}/>
+        <button className='bg-blue-500 text-white w-20 h-10 rounded-r-md' onClick={copyPasswordToClip}>Copy</button>
       </div>
       <div>
         <div>
