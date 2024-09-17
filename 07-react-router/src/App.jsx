7c0508e3791bd78a0b63dc3c25a2c869fd871d37
mallikaakash <mallik.aakash777@gmail.com>
2024-09-17 09:54:50 +0530

    setup 07

---
+++ 07-react-router/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-white'>React Router</h1>
+    </>
+  )
+}
+
+export default App
